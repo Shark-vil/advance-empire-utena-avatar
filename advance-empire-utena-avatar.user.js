99f097223e08dcf51fc5267c6949698afea9deaf
Shark-vil <shark_vil@mail.ru>
2024-02-11 21:18:21 +0300

    Update advance-empire-utena-avatar.user.js

--- advance-empire-utena-avatar.user.js
+++ advance-empire-utena-avatar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AlexandriА Utena Avatar
 // @namespace    https://shark.vincy.ru/
-// @version      2024-02-11
+// @version      2024-02-11:1
 // @description  Правильная аватарка.
 // @author       Shark_vil
 // @updateURL    https://github.com/Shark-vil/advance-empire-utena-avatar/raw/master/advance-empire-utena-avatar.user.js
@@ -9,76 +9,107 @@
 // @match        *://*.vk.com/im*
 // @match        *://*.vk.com/groups
 // @match        *://*.vk.com/advance_empire*
+// @match        *://*.vk.com/feed
+// @match        *://*.vk.com/feed?w=wall-34788285_*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 const regexReplace = /background-image\:.+url\(\'(.+)\?/;
 const rightAvatar = "https://i.ibb.co/JptVWP1/alexandria.png";
-const pictures = [
-    'https://sun1-16.userapi.com/s/v1/ig2/KWGkGCM9BdZ7yLKqHBTOSIxCkAfJJZyczwUhXuj-APTyIZTVFfSP6_Y3b6gKY0xxeQGW0jJu1yglT4cBn-6qxrYD.jpg',
-    'https://sun1-16.userapi.com/s/v1/ig2/bW5TpYYu6HfZ1J-uZ06vByxuxKjbXMKtnRhG_QDUYLQY0tCR0dFdwdgO_DdAmusfogBa1AicgoTTTK88ZienrkB3.jpg',
-    'https://sun1-16.userapi.com/impg/irwOFePCQt6s27yViJDvO00Bvp-ieUq63iarqQ/dX15e3ESvSo.jpg',
-    'https://sun1-16.userapi.com/s/v1/if2/uYLaiIi62sNnrADHmvHct6uaxwIF_ki2TmLfp9Ld_dxDvd7JG_v7HXzoH0RwSYz1G8sbhDlPdNj-stuqllAA1khO.jpg'
-];
 
-function imgFunc(img, index, collection)
-{
-    if (img._isReplacementAvatar == undefined && img.src != rightAvatar)
-    {
-        //console.log('[AAUA] check image - ' + img);
-        for (const picture of pictures) {
-            if (img.src.indexOf(picture) == 0) {
-                console.log('[AAUA] Replace image - ' + picture);
-                img._isReplacementAvatar = true;
-                img.src = rightAvatar;
-                break;
-            }
+function isValidImageToCahnge(element) {
+    if (element == undefined || element._isReplacementAvatar || element.src == rightAvatar) {
+        return false;
+    }
+    const altValue = element.getAttribute('alt');
+    if (altValue != undefined && altValue == 'AlexandriА (Аниме новости)') {
+        return true;
+    }
+    return false;
+}
+
+function isValidElementToCahnge(element) {
+    if (element == undefined || element._isReplacementAvatar) {
+        return false;
+    }
+    if (element.tagName == 'A') {
+        const hrefValue = element.getAttribute('href');
+        if (hrefValue == undefined || hrefValue.indexOf('/advance_empire') != 0) {
+            return false;
         }
+    }
+    return true;
+}
+
+function changeImageFunc(imgElement) {
+    if (isValidImageToCahnge(imgElement)) {
+        console.log('[AAUA] Replace image - ' + imgElement.src);
+        imgElement._isReplacementAvatar = true;
+        imgElement.src = rightAvatar;
     }
 }
 
-function owAvasFunc(divBlock, index, collection) {
-    if (divBlock._isReplacementAvatar == undefined)
-    {
-        //console.log('[AAUA] Check image div - ' + divBlock);
-        const styleValue = divBlock.getAttribute('style');
+function changeGeneralImageFunc(imgElement) {
+    if (isValidElementToCahnge(imgElement) && imgElement.src != rightAvatar) {
+        console.log('[AAUA] Replace image - ' + imgElement.src);
+        imgElement._isReplacementAvatar = true;
+        imgElement.src = rightAvatar;
+    }
+}
+
+function changeImageByClassFunc(element) {
+    if (isValidElementToCahnge(element)) {
+        const styleValue = element.getAttribute('style');
         if (styleValue == undefined) return;
         const matchValues = styleValue.match(regexReplace);
         if (matchValues != undefined && matchValues.length > 1) {
-            const styleUrlValue = matchValues[1];
-            for (const picture of pictures) {
-                if (picture == styleUrlValue) {
-                    console.log('[AAUA] Replace image style - ' + picture);
-                    divBlock._isReplacementAvatar = true;
-                    divBlock.setAttribute('style', styleValue.replace(picture, rightAvatar));
-                    break;
-                }
-            }
+            const urlValue = matchValues[1];
+            console.log('[AAUA] Replace image style - ' + urlValue);
+            element._isReplacementAvatar = true;
+            element.setAttribute('style', styleValue.replace(urlValue, rightAvatar));
         }
     }
 }
 
-function updateImageSources()
-{
-    const allImages = document.querySelectorAll('img');
-    allImages.forEach(imgFunc);
-
-    const owAvas = document.querySelectorAll('.ow_ava,.Avatar__image');
-    owAvas.forEach(owAvasFunc);
+function changeNoNameImageFunc(element) {
+    if (isValidElementToCahnge(element)) {
+        element._isReplacementAvatar = true;
+        const allImages = element.querySelectorAll('img');
+        allImages.forEach(changeGeneralImageFunc);
+    }
 }
 
-function callbackFunc(mutationsList, observer)
-{
+function updateImageSources() {
+    // const generalAvatarImage = document.getElementById('pv_photo');
+    // if (generalAvatarImage != undefined && window.location.href.indexOf('vk.com/advance_empire') == 0) {
+    //     const allGeneralAvatarImages = document.querySelectorAll('img');
+    //     const imgElement = allGeneralAvatarImages[0];
+    //     if (imgElement != undefined) {
+    //         console.log('[AAUA] Replace image - ' + imgElement.src);
+    //         imgElement._isReplacementAvatar = true;
+    //         imgElement.src = rightAvatar;
+    //     }
+    // }
+
+    const allImages = document.querySelectorAll('img');
+    allImages.forEach(changeImageFunc);
+
+    const allImagesByClass = document.querySelectorAll('.ow_ava,.Avatar__image');
+    allImagesByClass.forEach(changeImageByClassFunc);
+
+    const allNoNameImages = document.querySelectorAll('a');
+    allNoNameImages.forEach(changeNoNameImageFunc);
+}
+
+function callbackFunc(mutationsList, observer) {
     console.log('[AAUA] Listen images created');
-    for (const mutation of mutationsList)
-    {
+    for (const mutation of mutationsList) {
         if (mutation.type == 'childList') updateImageSources();
     }
 }
 
-function onWindowLoaded(evt)
-{
+function onWindowLoaded(evt) {
     console.log('[AAUA] Init - AlexandriА Utena Avatar');
     const targetNode = document.body;
     const config = {attributes:true, childList:true, subtree: true};
